08083086c102d49387f5ca27b1919e5c90515706
tzlzht520 <409971398@qq.com>
2019-04-29 17:03:59 +0800

    测试获取code

---
+++ SanyaShowPhoto/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>{{res}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "about",
+  data() {
+    return {
+      code: this.$route.param.code,
+      res: ""
+    }
+  },
+  methods: {
+    getBaseInfo(){
+      let params = {
+        code: this.code
+      }
+      this.axios.post("http://wxsc.saselomo.com/index.php?sanya-wxlogin",params).then(res => {
+        this.res = res
+      })
+    }
+  },
+}
+</script>
---
+++ SanyaShowPhoto/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+      code: this.$route.param.code,
+      redirect_uri: encodeURI("http://wxsc.saselomo.com/about"),
+      appid: "wx55da418f69963f69"
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    getCod(){
+      if(this.code){
+        alert("错误")
+      }else {
+        console.log(location)
+        // window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+ this.appid +"&redirect_uri="+ this.redirect_uri +"&type=code&scope=snsapi_base#wechat_redirect"
+      }
+    }
+  },
+  created() {
+    this.getCod()
+  },
+}
+</script>
